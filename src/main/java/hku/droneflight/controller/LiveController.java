b5ccf76fbb7a6a308cfd273a01a7012e7d395a9f
Victory <47774751+victory66666666@users.noreply.github.com>
2022-07-01 17:59:00 +0800

    update api

--- src/main/java/hku/droneflight/controller/LiveController.java
+++ src/main/java/hku/droneflight/controller/LiveController.java
@@ -3,6 +3,7 @@ package hku.droneflight.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import hku.droneflight.entity.User;
 import hku.droneflight.entity.Video;
+import hku.droneflight.service.UserService;
 import hku.droneflight.service.VideoService;
 import hku.droneflight.util.*;
 
@@ -23,10 +24,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LiveController {
 
     static List<String> streamUrls = new ArrayList<>();
-    static Map<String,Integer> streamVideoMap = new HashMap<>();
-    static Map<String,String> streamResultMap = new HashMap<>();
+    static Map<String, Integer> streamVideoMap = new HashMap<>();
+    static Map<String, String> streamResultMap = new HashMap<>();
     static Map<String, User> streamUserMap = new HashMap<>();
 
+    @Autowired
+    UserService userService;
 
     @Autowired
     VideoService videoService;
@@ -36,33 +39,36 @@ public class LiveController {
 
     /**
      * 开始直播接口，接收app发送的视频信息和推流地址
+     *
      * @param liveReq
      */
     @RequestMapping(value = "/startLive")
     @ResponseBody
-    ResponseMsg startLive(@RequestBody LiveReq liveReq){
+    ResponseMsg startLive(@RequestBody LiveReq liveReq) {
         streamUrls.add(liveReq.streamUrl);
-        User user = liveReq.user;
-        streamUserMap.put(liveReq.streamUrl,user);
+        Integer userId = liveReq.user.getId();
+        User user = userService.getUserById(userId);
+        streamUserMap.put(liveReq.streamUrl, user);
         return new ResponseMsg(Result.SUCCESS);
     }
 
     /**
      * 查询是否有直播，返回第一个streamUrl和resultUrl
      * Result.FAIL表示没有直播
+     *
      * @return
      */
     @RequestMapping(value = "/isLiveStart")
     @ResponseBody
-    UrlRsp isLiveStart(){
-        if (streamUrls.isEmpty()){
+    UrlRsp isLiveStart() {
+        if (streamUrls.isEmpty()) {
             return new UrlRsp(Result.FAIL, "no live!");
-        } else if (streamResultMap.containsKey(streamUrls.get(0))){
+        } else if (streamResultMap.containsKey(streamUrls.get(0))) {
             UrlRsp urlRsp = new UrlRsp(Result.SUCCESS);
             urlRsp.streamUrl = streamUrls.get(0);
             urlRsp.resultUrl = streamResultMap.get(urlRsp.streamUrl);
             return urlRsp;
-        } else{
+        } else {
             UrlRsp urlRsp = new UrlRsp(Result.SUCCESS);
             urlRsp.streamUrl = streamUrls.get(0);
             User user = streamUserMap.get(urlRsp.streamUrl);
@@ -78,19 +84,20 @@ public class LiveController {
                 sb.append("/");
             }
             urlRsp.resultUrl = sb.substring(0, sb.length() - 2);
-            streamResultMap.put(urlRsp.streamUrl,urlRsp.resultUrl);
+            streamResultMap.put(urlRsp.streamUrl, urlRsp.resultUrl);
             return urlRsp;
         }
     }
 
     /**
      * 停止直播，不保存信息
+     *
      * @param liveReq
      * @return
      */
     @RequestMapping(value = "/stopLive")
     @ResponseBody
-    ResponseMsg stopLive(@RequestBody LiveReq liveReq){
+    ResponseMsg stopLive(@RequestBody LiveReq liveReq) {
         //加个校验
         streamUrls.remove(liveReq.streamUrl);
         streamResultMap.remove(liveReq.streamUrl);
@@ -100,13 +107,14 @@ public class LiveController {
 
     /**
      * 存储视频信息和删除streamUrl，记录vid
+     *
      * @param videoReq
      * @return
      */
     @RequestMapping(value = "/stopAndSaveLive")
     @ResponseBody
-    ResponseMsg stopAndSaveLive(@RequestBody VideoReq videoReq){
-        if (videoReq.streamUrl != null){
+    ResponseMsg stopAndSaveLive(@RequestBody VideoReq videoReq) {
+        if (videoReq.streamUrl != null) {
             streamUrls.remove(videoReq.streamUrl);
             streamResultMap.remove(videoReq.streamUrl);
             User user = streamUserMap.remove(videoReq.streamUrl);
@@ -129,12 +137,13 @@ public class LiveController {
     /**
      * 查询指定streamUrl直播是否停止
      * Result.FAIL表示该url仍在直播
+     *
      * @return
      */
     @RequestMapping(value = "/isLiveStop")
     @ResponseBody
-    LiveListRsp isLiveStop(@RequestBody LiveReq liveReq){
-        if(streamUrls.contains(liveReq.streamUrl))
+    LiveListRsp isLiveStop(@RequestBody LiveReq liveReq) {
+        if (streamUrls.contains(liveReq.streamUrl))
             return new LiveListRsp(Result.FAIL);
         else
             return new LiveListRsp(Result.SUCCESS);
@@ -142,68 +151,62 @@ public class LiveController {
 
     /**
      * 获取所有直播url
+     *
      * @return
      */
     //加个list UrlRsp
     //加个user
     @RequestMapping(value = "/getLive")
     @ResponseBody
-    UrlListRsp getLiveUrlList(){
+    UrlListRsp getLiveUrlList() {
         List<UrlRsp> urlRsps = new ArrayList<>();
-        for(String streamUrl:streamUrls){
+        for (String streamUrl : streamUrls) {
             UrlRsp urlRsp = new UrlRsp(Result.SUCCESS);
-            urlRsp.streamUrl =  streamUrl;
-            urlRsp.resultUrl =  streamResultMap.get(streamUrl);
+            urlRsp.streamUrl = streamUrl;
+            urlRsp.resultUrl = streamResultMap.get(streamUrl);
             urlRsp.user = streamUserMap.get(streamUrl);
             urlRsps.add(urlRsp);
         }
         UrlListRsp urlListRsp = new UrlListRsp(Result.SUCCESS);
-        urlListRsp.urlRspList=urlRsps;
+        urlListRsp.urlRspList = urlRsps;
         return urlListRsp;
     }
 
     /**
      * 获取所有本地视频
+     *
      * @return
      */
     @RequestMapping(value = "/getVideo")
     @ResponseBody
-    VideoListRsp getRecordUrlList(Integer uid){
+    VideoListRsp getRecordUrlList(Integer uid) {
         List<Video> videos = videoService.getListByUid(uid);
-        if(videos.size()>0){
-            VideoListRsp videoListRsp =new VideoListRsp(Result.SUCCESS);
-            videoListRsp.videoList=videos;
+        if (videos.size() > 0) {
+            VideoListRsp videoListRsp = new VideoListRsp(Result.SUCCESS);
+            videoListRsp.videoList = videos;
+            videoListRsp.user=userService.getUserById(uid);
             return videoListRsp;
-        }
-        else{
-            return new VideoListRsp(Result.FAIL,"没有本地视频");
-        }
-    }
-
-    /**
-     * 根据url播放指定视频
-     * @return
-     */
-    @RequestMapping(value = "/playRecord")
-    @ResponseBody
-    VideoRsp playRecordVideo(String url){
-        LambdaQueryWrapper<Video> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Video::getUrl,url);
-        Video video = videoService.getOne(queryWrapper);
-        if(video==null){
-            return new VideoRsp(Result.FAIL,"链接地址无效！");
-
-        }
-        else{
-            VideoRsp videoRsp= new VideoRsp(Result.SUCCESS);
-            videoRsp.video=video;
-            return videoRsp;
+        } else {
+            return new VideoListRsp(Result.FAIL, "没有本地视频");
         }
     }
-
-
-
-
-
-
+//    /**
+//     * 根据url播放指定视频
+//     * @return
+//     */
+//    @RequestMapping(value = "/playRecord")
+//    @ResponseBody
+//    VideoRsp playRecordVideo(String url){
+//        LambdaQueryWrapper<Video> queryWrapper = new LambdaQueryWrapper<>();
+//        queryWrapper.eq(Video::getUrl,url);
+//        Video video = videoService.getOne(queryWrapper);
+//        if(video==null){
+//            return new VideoRsp(Result.FAIL,"链接地址无效！");
+//        }
+//        else{
+//            VideoRsp videoRsp= new VideoRsp(Result.SUCCESS);
+//            videoRsp.video=video;
+//            return videoRsp;
+//        }
+//    }
 }
